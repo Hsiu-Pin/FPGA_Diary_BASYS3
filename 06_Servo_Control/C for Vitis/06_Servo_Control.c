9660352709a86c6505ef1ef4e7a0d800bc217199
Hsiu-Pin <pinpinhsu18@gmail.com>
2024-09-10 13:52:43 +0100

    Create 06_Servo_Control.c

---
+++ 06_Servo_Control/C for Vitis/06_Servo_Control.c
@@ -0,0 +1,56 @@
+/******************************************************************************
+* Author: Hsiu-Pin Hsu
+* Date: 2024.08.24
+******************************************************************************/
+
+#include <stdio.h>
+#include "platform.h"
+#include "xil_printf.h"
+#include "xparameters.h"
+#include "xil_types.h"
+#include "xil_io.h"
+
+#define DEG XPAR_DEGREE_BASEADDR
+
+u32 deg;
+
+int main()
+{
+    init_platform();
+
+    xil_printf("Program Start !!!\n\r \n\r");
+
+    // Initial Value
+    Xil_Out32(DEG, 0x00000000);
+
+    deg = 0;
+
+    while(1){
+
+        // Detect ASCII from computer keyboard
+        deg = XUartLite_RecvByte(XPAR_XUARTLITE_0_BASEADDR);
+
+        // ================================================
+        // Valid Number 0~7
+        // 0: IDLE
+        // 1~7: 0-deg, 30, 60, 90, 120, 150, 180
+        if(deg <=55 && deg >= 49) { // Valid = 1~7
+            deg = deg-48; // ASCII -> Decimal
+            if     (deg==1){ xil_printf("  0 degrees \n\r"); }
+            else if(deg==2){ xil_printf(" 30 degrees \n\r"); }
+            else if(deg==3){ xil_printf(" 60 degrees \n\r"); }
+            else if(deg==4){ xil_printf(" 90 degrees \n\r"); }
+            else if(deg==5){ xil_printf("120 degrees \n\r"); }
+            else if(deg==6){ xil_printf("150 degrees \n\r"); }
+            else if(deg==7){ xil_printf("180 degrees \n\r"); }
+            Xil_Out32(DEG, deg);
+        // ================================================
+        } else {
+            xil_printf("Invalid Input... \n\r \n\r");
+        }
+
+    }
+
+    cleanup_platform();
+    return 0;
+}
