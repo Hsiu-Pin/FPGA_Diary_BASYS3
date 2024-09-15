abb5dbb8c5a53881e12ecb66409ee8f1f483ca97
Hsiu-Pin <pinpinhsu18@gmail.com>
2024-09-15 12:02:48 +0100

    Update 07_Bluetooth_Control.c

--- 07_Bluetooth_Control/C for Vitis/07_Bluetooth_Control.c
+++ 07_Bluetooth_Control/C for Vitis/07_Bluetooth_Control.c
@@ -11,9 +11,11 @@
 #include "xil_io.h"
 
 #define BA_DEG     XPAR_SERVOCONTROL_WRAP_0_BASEADDR
-#define BA_BT_RX   XPAR_BLUETOOTHCONTROL_WRAP_0_BASEADDR
+#define BA_BT_TX   XPAR_BLUETOOTHCONTROL_WRAP_0_BASEADDR
+#define BA_BT_RX   XPAR_BLUETOOTHCONTROL_WRAP_0_BASEADDR + 4
 
 u32 deg;
+u32 deg_r;
 
 int main()
 {
@@ -28,27 +30,41 @@ int main()
 
     while(1){
 
+        // ================================================
         // Detect ASCII from bluetooth module
         deg = Xil_In32(BA_BT_RX);
 
         // ================================================
-        // Valid Number 0~7
-        // 0: IDLE
-        // 1~7: 0-deg, 30, 60, 90, 120, 150, 180
-        if(deg <=55 && deg >= 49) { // Valid = 1~7
-            deg = deg-48; // ASCII -> Decimal
-            if     (deg==1){ xil_printf("  0 degrees \n\r"); }
-            else if(deg==2){ xil_printf(" 30 degrees \n\r"); }
-            else if(deg==3){ xil_printf(" 60 degrees \n\r"); }
-            else if(deg==4){ xil_printf(" 90 degrees \n\r"); }
-            else if(deg==5){ xil_printf("120 degrees \n\r"); }
-            else if(deg==6){ xil_printf("150 degrees \n\r"); }
-            else if(deg==7){ xil_printf("180 degrees \n\r"); }
-            Xil_Out32(BA_DEG, deg);
-        // ================================================
-        } else {
-            xil_printf("Invalid Input... \n\r \n\r");
+        // Using Tx to send back the input degree
+        if(deg!=deg_r){
+            // for same degree, only send once
+            deg_r  = deg;
+
+            // ================================================
+            // Trigger the Servo
+            // Valid Number 0~7
+            // 0: IDLE
+            // 1~7: 0-deg, 30, 60, 90, 120, 150, 180
+            if(deg <=55 && deg >= 49) { 
+                Xil_Out32(BA_DEG, deg-48); // -48 to convert to ASCII to Decimal
+            } 
+
+            // ================================================
+            // Trigger the Bluetooth Tx
+            // slv_reg_0[0]    = tx_valid
+            // slv_reg_0[15:8] = tx_data
+            // +1 to trigger the valid signaln
+            deg    = (deg << 8) + 1;; 
+            
+            Xil_Out32(BA_BT_TX, deg);
+            
+            usleep(100);
+            // clean the valid
+            Xil_Out32(BA_BT_TX, 0x00000000); 
         }
+        // ================================================
+        
+        
 
     }
 
